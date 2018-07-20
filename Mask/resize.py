68d1341efdc83246d147c503188c87d251e9ff99
SolmazJaunt <36427318+SolmazJaunt@users.noreply.github.com>
2018-07-19 20:23:19 -0700

    Add files via upload

---
+++ Mask/resize.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np,sys
+def main():
+    image = cv2.imread(sys.argv[1])
+    resized = cv2.resize(image, (640, 480))
+    cv2.imwrite('Direct_blending.jpg',resized)
+
+if __name__ == "__main__":
+    main()
