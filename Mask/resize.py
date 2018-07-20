6e7e47ee96de8bbef53f59518d3d707e674d9746
SolmazJaunt <36427318+SolmazJaunt@users.noreply.github.com>
2018-07-19 20:27:43 -0700

    Update resize.py

--- Mask/resize.py
+++ Mask/resize.py
@@ -3,7 +3,7 @@ import numpy as np,sys
 def main():
     image = cv2.imread(sys.argv[1])
     resized = cv2.resize(image, (640, 480))
-    cv2.imwrite('Direct_blending.jpg',resized)
+    cv2.imwrite(sys.argv[1],resized)
 
 if __name__ == "__main__":
     main()
